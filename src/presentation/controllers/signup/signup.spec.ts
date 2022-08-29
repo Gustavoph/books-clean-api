3b136c4609e8d65997e1454f26cbbc3a5aeece8b
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-08-29 18:08:31 -0300

    test: ensure Sign Up Controller return 400 if password confirmation does not match password

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from './signup'
 import { HttpRequest } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
+import { InvalidParamError } from '../../errors/invalid-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if name is not provided', async () => {
@@ -61,5 +62,21 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('Should return 500 if password confirmation does not match password', async () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'another_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
   })
 })
